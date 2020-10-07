320f254aa8acdbc994a8859b3f22af943d8d0b47
Yngwie Mariman <yngwie.mariman@vdabcampus.be>
2020-10-07 15:52:51 +0200

    FrituurApplication

---
+++ src/main/java/be/vdab/frituur/FrituurApplication.java
@@ -0,0 +1,14 @@
+package be.vdab.frituur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrituurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrituurApplication.class, args);
+    }
+
+}
+
