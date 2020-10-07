721b636cceeb8ceb051fe1a6a0e477f18866348a
Yngwie Mariman <yngwie.mariman@vdabcampus.be>
2020-10-07 15:54:02 +0200

    FrituurApplicationTests

---
+++ src/test/java/be/vdab/frituur/FrituurApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.frituur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrituurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
